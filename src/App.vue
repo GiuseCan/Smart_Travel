b08087e6d4246475d05c0b5b9d2d224365041cd6
Giuse Can <canbl2002@gmail.com>
2024-02-19 09:16:48 +0700

    version_1

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-24 bg-blue-500 flex justify-center items-center">
+    <h1 class="text-white font-bold text-lg">
+      Smart Travel
+    </h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
