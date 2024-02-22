a833ad7fdedc5a2f92687ec1e84c3e8c9f4c6923
Giuse Can <canbl2002@gmail.com>
2024-02-22 22:14:12 +0700

    update About

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,9 +1,227 @@
 <template>
-    <div>
-        <h1>AboutUs</h1>
+  <div>
+    <Services />
+
+    <div id="aboutus">
+      <section class="pb-14 flex flex-col items-center gap-5">
+        <h5 class="text-sm">SIMPLY AMAZING PLACES</h5>
+        <h4 class="text-3xl font-bold">A few words about us</h4>
+      </section>
+      <div class="flex justify-center px-44 gap-10">
+        <div class="" id="text_about">
+          <h4 class="font-bold text-xl pb-7">
+            Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+            tristique at leo. Vivamus massa.Tempor massa et laoreet .Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit.
+          </h4>
+          <p class="text-gray-500">
+            Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+            tristique at leo. Vivamus massa.Tempor massa et laoreet .Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Sed eu laoreet ante,
+            sollicitudin volutpat quam. Vestibulum posuere malesuada ultrices.
+            In pulvinar rhoncus lacus at aliquet. Nunc vitae lacus varius,
+            auctor nisi sit amet, consectetur mauris. Curabitur sodales semper
+            est, vel faucibus urna laoreet vel. Ut justo diam, sodales non
+            pulvinar at, vulputate quis neque. Etiam aliquam purus vel ultricies
+            consequat.
+          </p>
+          <button
+            class="mt-10 bg-slate-500 py-3 px-8 rounded-xl bg-slate-900 text-lg text-gray-200"
+          >
+            READ MORE
+          </button>
+        </div>
+        <div>
+          <img
+            id="sontra"
+            src="https://preview.colorlib.com/theme/travello/images/about_1.jpg.webp"
+            alt="sontra"
+          />
+        </div>
+      </div>
+      <div class="flex justify-center gap-40 mt-20">
+        <section class="flex flex-col items-center gap-2">
+          <img
+            class="w-20 h-20"
+            src="https://preview.colorlib.com/theme/travello/images/mountain.svg"
+            alt="aboutus"
+          />
+          <h5 class="text-4xl font-bold">17</h5>
+          <h6 class="font-bold text-gray-500">Online Courses</h6>
+        </section>
+        <section class="flex flex-col items-center gap-2">
+          <img
+            class="w-20 h-20"
+            src="https://preview.colorlib.com/theme/travello/images/mountain.svg"
+            alt="aboutus"
+          />
+          <h5 class="text-4xl font-bold">17</h5>
+          <h6 class="font-bold text-gray-500">Online Courses</h6>
+        </section>
+        <section class="flex flex-col items-center gap-2">
+          <img
+            class="w-20 h-20"
+            src="https://preview.colorlib.com/theme/travello/images/island.svg"
+            alt="aboutus"
+          />
+          <h5 class="text-4xl font-bold">213</h5>
+          <h6 class="font-bold text-gray-500">Customer</h6>
+        </section>
+        <section class="flex flex-col items-center gap-2">
+          <img
+            class="w-20 h-20"
+            src="https://preview.colorlib.com/theme/travello/images/boat.svg    "
+            alt="aboutus"
+          />
+          <h5 class="text-4xl font-bold">15</h5>
+          <h6 class="font-bold text-gray-500">City</h6>
+        </section>
+      </div>
+      <div
+        class="mt-20 bg-[url('../../image//bg-aboutus.jpg.webp')] w-full h-[760px] bg-cover bg-center bg-no-repeat mb-8"
+      >
+        <section class="mb-20 flex flex-col items-center gap-6 pt-10">
+          <h5 class="text-gray-100">SIMPLY AMAZING PLACES</h5>
+          <h4 class="text-gray-100 font-bold text-5xl">Why choose us?</h4>
+        </section>
+        <div class="flex justify-around px-36" id="whychoose">
+          <div
+            class="shadow-xl bg-gray-100 flex flex-col items-center gap-4"
+            id="chooseus"
+          >
+            <img src="../../image/Love_Bridge.jpg" alt="img" />
+            <h3 class="pt-2 font-bold text-gray-950 text-2xl">Fast Services</h3>
+            <p class="text-gray-500 text-center px-5">
+              Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+              tristique at leo.
+            </p>
+          </div>
+          <div
+            class="shadow-xl bg-gray-100 flex flex-col items-center gap-4"
+            id="chooseus"
+          >
+            <img src="../../image/Cau_Rong_1.jpg" alt="img" />
+            <h3 class="pt-2 font-bold text-gray-950 text-2xl">Great Team</h3>
+            <p class="text-gray-500 text-center px-5">
+              Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+              tristique at leo.
+            </p>
+          </div>
+          <div
+            class="shadow-xl bg-gray-100 flex flex-col items-center gap-4"
+            id="chooseus"
+          >
+            <img src="../../image/Thuan_Phuo_Bridge.jpg" alt="img" />
+            <h3 class="pt-2 font-bold text-gray-950 text-2xl">Best Deals</h3>
+            <p class="text-gray-500 text-center px-5">
+              Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+              tristique at leo.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="pt-8">
+        <section class="pb-14 flex flex-col items-center gap-5">
+          <h5 class="text-sm">SIMPLY AMAZING PLACES</h5>
+          <h4 class="text-3xl font-bold">Meet the Team</h4>
+        </section>
+        <div id="meetteam" class="flex justify-center pb-24 gap-5">
+          <div class="w-64 flex flex-col items-center gap-5">
+            <img
+              src="../../image/avt.jpg"
+              class="w-44 h-44 object-cover rounded-full"
+              alt="avt"
+            />
+            <h4 class="text-xl font-bold">Cẩn Cris</h4>
+            <p class="text-gray-500 text-center" >
+              Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+              tristique at leo. Vivamus massa.
+            </p>
+          </div>
+          <div class="w-64 flex flex-col items-center gap-5">
+            <img
+              src="../../image/avt.jpg"
+              class="w-44 h-44 object-cover rounded-full"
+              alt="avt"
+            />
+            <h4 class="text-xl font-bold">Cẩn Cris</h4>
+            <p class="text-gray-500 text-center" >
+              Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+              tristique at leo. Vivamus massa.
+            </p>
+          </div>
+          <div class="w-64 flex flex-col items-center gap-5">
+            <img
+              src="../../image/avt.jpg"
+              class="w-44 h-44 object-cover rounded-full"
+              alt="avt"
+            />
+            <h4 class="text-xl font-bold">Cẩn Cris</h4>
+            <p class="text-gray-500 text-center" >
+              Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+              tristique at leo. Vivamus massa.
+            </p>
+          </div>
+          <div class="w-64 flex flex-col items-center gap-5">
+            <img
+              src="../../image/avt.jpg"
+              class="w-44 h-44 object-cover rounded-full"
+              alt="avt"
+            />
+            <h4 class="text-xl font-bold">Cẩn Cris</h4>
+            <p class="text-gray-500 text-center" >
+              Pellentesque sit amet elementum ccumsan sit amet mattis eget,
+              tristique at leo. Vivamus massa.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-
+import Services from "./Services.vue";
 </script>
+
+<style scoped>
+img#sontra {
+  width: 558px;
+  height: 518px;
+  background-image: cover;
+}
+div#aboutus {
+  margin-top: 250px;
+}
+div#text_about {
+  width: 550px;
+}
+div#chooseus {
+  width: 362px;
+  height: 408px;
+}
+div#whychoose > div {
+  position: relative;
+}
+
+div#whychoose > div::after {
+  content: "";
+  position: absolute;
+  width: 70px;
+  height: 70px;
+  background-color: rgb(241, 241, 241);
+  top: 0;
+  transform: translateY(-50%);
+  border-radius: 50%;
+  background-image: url("https://preview.colorlib.com/theme/travello/images/why_1.svg");
+  background-repeat: no-repeat;
+  background-size: 50px 50px;
+  background-position: center center;
+}
+div#whychoose > div:nth-child(2):after {
+  background-image: url("https://preview.colorlib.com/theme/travello/images/why_2.svg");
+}
+div#whychoose > div:nth-child(3):after {
+  background-image: url("https://preview.colorlib.com/theme/travello/images/why_3.svg");
+}
+</style>
